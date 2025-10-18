383acdbfa16b36f1545be8df03f10c16affa9eaf
Omar Rodriguez-Bernal <20omarr04@gmail.com>
2025-10-18 15:04:13 -0700

    created button and api request/return methods for button

--- my_assignment/lib/main.dart
+++ my_assignment/lib/main.dart
@@ -2,8 +2,9 @@
     main.dart
 */
 import 'package:flutter/material.dart';
-import 'package:flutter_spinbox/flutter_spinbox.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert'; // for decoding json formatted text
+import 'package:http/http.dart' as http; // for making the http request
 
 void main() => runApp(MainPage());
 
@@ -13,19 +14,25 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
+//the following class is for storing information returned in the api request to oepnfoodfacts
+class FoodCard {
+  String? name;
+  String? ingredients;
+  Map<String, dynamic>? nutriments;
+  List<dynamic>? allergenTags;
+  List<dynamic>? traces;
+
+  FoodCard({
+    required this.name,
+    required this.ingredients,
+    required this.nutriments,
+    required this.allergenTags,
+  });
+}
+
 class _MainPageState extends State<MainPage> {
   //Appbar Color
   final Color _appBarColor = Colors.redAccent;
-
-  //Controllers for retrieving input
-  final TextEditingController _textController = TextEditingController();
-  double tempFavNumber = 0;
-  int tempId = 1;
-
-  //Variables for input storage:
-  String name = "";
-  double favNumber = 0;
-  int id = 0;
 
   //Functions assosiated with the shared_preferences function:
   late SharedPreferences prefs;
@@ -33,117 +40,88 @@ class _MainPageState extends State<MainPage> {
   @override
   void initState() {
     super.initState();
-    _initPrefs(); // Call the async function here
   }
 
-  Future<void> _initPrefs() async {
-    prefs = await SharedPreferences.getInstance();
-    _loadPrefs();
+  Future<Map<String, dynamic>?> retrieveFoodInformation(String barcode) async {
+    /*
+      URL Construction:
+        The base url for OpenFoodFacts is the inital url.
+        The barcode and query parameters are attached to the final url.
+  */
+
+    final Uri uri =
+        Uri.parse(
+          'https://world.openfoodfacts.org/api/v2/product/$barcode.json',
+        ).replace(
+          queryParameters: {
+            'fields':
+                'product_name,code,ingredients_text,nutriments, allergens,allergens_tags,traces_tags',
+          },
+        );
+
+    /*
+      HTTP Response Handling:
+        Null will be returned in the case that the foodscan server does not respond or information pertaining to the food item is not found.
+  */
+    final response = await http.get(
+      uri,
+      headers: {'User-Agent': 'FoodScan/1.0 (20omarr04@gmail.com)'},
+    );
+
+    if (response.statusCode != 200) {
+      return null;
+    }
+    final Map<String, dynamic> json = jsonDecode(response.body);
+
+    if (json['status'] != 1) {
+      return null;
+    }
+    final product = json['product'] as Map<String, dynamic>;
+    final ingredients = product['ingredients_text'];
+    final nutriments = product['nutriments'] as Map<String, dynamic>?;
+    final allergenTags = product['allergens_tags'] as List<dynamic>?;
+    final traces = product['traces_tags'] as List<dynamic>?;
+    final foodName = product['product_name'] as String? ?? 'Unknown Food';
+
+    return {
+      'food_name': foodName,
+      'ingredients': ingredients,
+      'nutriments': nutriments,
+      'allergen_tags': allergenTags,
+      'traces': traces,
+    };
   }
 
-  Future<void> _loadPrefs() async {
-    setState(() {
-      name = prefs.getString('name') ?? '';
-      favNumber = prefs.getDouble('favNumber') ?? 0.0;
-      id = prefs.getInt('id') ?? 1;
-    });
-  }
+  List<Map<String, dynamic>?> foodReturnValues = [];
 
-  //Functions for input handling
-  Future<void> saveInputValues() async {
-    final testName = _textController.text;
-    final testFavNumber = tempFavNumber;
-    final testId = tempId;
+  Future<void> addItemsToList() async {
+    List<String> foodItemUpcCodes = [
+      "028400047913", //hot cheetoes
+      "074323092301", //bimbo whole wheat
+      "888109253097", //jumbo honey bun
+    ];
 
-    final savedName = prefs.getString('name') ?? '';
-    final savedFavNumber = prefs.getDouble('favNumber') ?? 0.0;
-    final savedId = prefs.getInt('id') ?? 1;
-
-    bool hasChanged = false;
-
-    //name is only updated when it has a value and is a different value
-    if (testName.isNotEmpty && testName != savedName) {
-      await prefs.setString('name', testName);
-      hasChanged = true;
+    foodReturnValues.clear();
+    for (var item in foodItemUpcCodes) {
+      var itemToAdd = await retrieveFoodInformation(item); // wait for result
+      if (itemToAdd != null) {
+        // only items that have return value returned.
+        foodReturnValues.add(itemToAdd);
+      }
     }
-
-    if (testFavNumber != savedFavNumber) {
-      await prefs.setDouble('favNumber', testFavNumber);
-      hasChanged = true;
-    }
-
-    if (testId != savedId) {
-      await prefs.setInt('id', testId);
-      hasChanged = true;
-    }
-
-    if (hasChanged) {
-      setState(() {
-        if (testName.isNotEmpty) {
-          name = testName;
-        }
-        favNumber = testFavNumber;
-        id = testId;
-      });
-    }
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     List<Widget> widgetList = [
-      /*
-        method of displaying name, favorite number, and id
-    */
-      //name: text
-      Text('Current name: $name'),
-      Text('Current favorite number: $favNumber'),
-      Text('Current id: $id'),
-
-      /*
-      form with four elements:
-        textfield for name,
-        scroll for favorite number,
-        number text field? for id
-        submit button
-    */
-      TextField(
-        controller: _textController,
-        decoration: InputDecoration(labelText: "Name input"),
-      ),
-
-      SpinBox(
-        min: -100.0,
-        max: 100.0,
-        value: tempFavNumber,
-        step: 0.1,
-        decimals: 2,
-        decoration: const InputDecoration(labelText: 'Favorite Number'),
-        onChanged: (val) {
-          setState(() => tempFavNumber = val);
-        },
-      ),
-
-      SpinBox(
-        min: 1,
-        max: 5000,
-        value: tempId.toDouble(),
-        step: 1,
-        decimals: 0,
-        decoration: const InputDecoration(labelText: 'Id'),
-        onChanged: (val) {
-          setState(() => tempId = val.toInt());
-        },
-      ),
-
-      //button that when pressed, saves all of the information to a persistent state
       ElevatedButton(
         onPressed: () async {
-          // Action when the button is pressed
-          await saveInputValues();
-          await _loadPrefs();
+          await addItemsToList();
         },
-        child: Text('Press to store current values'),
+        child: Text('Press to load items'),
       ),
+      Expanded(child: DisplayItems(itemsToShow: foodReturnValues)),
     ];
 
     return MaterialApp(
@@ -157,6 +135,59 @@ class _MainPageState extends State<MainPage> {
           child: Column(children: widgetList),
         ),
       ),
+    );
+  }
+}
+
+class DisplayItems extends StatelessWidget {
+  final List<Map<String, dynamic>?> itemsToShow;
+
+  const DisplayItems({required this.itemsToShow});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: itemsToShow.length,
+      itemBuilder: (context, index) {
+        final item = itemsToShow[index];
+
+        //null check
+        if (item == null) {
+          return ListTile(title: Text("No data available"));
+        }
+
+        return Card(
+          margin: EdgeInsets.all(8.0),
+          child: ListTile(
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              //elements from api request are retrieved
+              children: [
+                Text(
+                  item['food_name'] ?? 'Unknown Food',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  "Ingredients: ${item['ingredients'] ?? 'Not available'}",
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(height: 8.0),
+
+                if (item['allergen_tags'] != null &&
+                    item['allergen_tags']!.isNotEmpty)
+                  Text(
+                    "Allergens: ${item['allergen_tags']?.join(', ') ?? 'None'}",
+                    style: TextStyle(fontSize: 16),
+                  )
+                else
+                  Text("Allergens: None", style: TextStyle(fontSize: 16)),
+                SizedBox(height: 8.0),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
